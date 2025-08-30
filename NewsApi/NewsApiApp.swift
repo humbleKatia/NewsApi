84930c75126eff72287b15c25bb0d3e39978853b
humbleKatia <ekaterinka.lysova74@gmail.com>
2025-08-30 11:10:26 +0100

    Initial Commit

---
+++ NewsApi/NewsApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApiApp.swift
+//  NewsApi
+//
+//  Created by Ekaterina Lysova on 30/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct NewsApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
